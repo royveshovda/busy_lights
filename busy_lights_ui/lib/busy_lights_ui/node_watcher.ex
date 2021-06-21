98f0c96ccbee951887d62397b265d665adf7b3d4
Roy Veshovda <roy@veshovda.no>
2021-06-21 12:54:33 +0200

    Prepare for node watcher

---
+++ busy_lights_ui/lib/busy_lights_ui/node_watcher.ex
@@ -0,0 +1,9 @@
+defmodule BusyLightsUi.FakeLights do
+  require Logger
+
+  #:net_kernel.monitor_nodes(:true,[])
+  #{:nodeup, :"nerves@10.223.80.104"}
+  #{:nodedown, :"nerves@10.223.80.103"}
+
+
+end
