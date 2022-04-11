31c4afd1c91614bf0bedcdeefc8c17d30f6f2fbb
Roy Veshovda <roy@veshovda.no>
2022-04-11 19:46:57 +0200

    adjust to upgraded nerves syntax

--- prepare.sh
+++ prepare.sh
@@ -1,15 +1,11 @@
 export MIX_ENV=prod
 cd busy_lights_ui
 mix deps.get
-npm install --prefix assets
-npm install --prefix assets --production
-npm run deploy --prefix assets
-mix phx.digest
+mix assets.deploy
 cd ../busy_lights_fw
 export MIX_TARGET=rpi0
 mix deps.get
-mix compile
 #mix firmware
 echo "Remeber to run: export NERVES_NETWORK_SSID=your_wifi_name and "
 echo "export NERVES_NETWORK_PSK=your_wifi_password"
-echo "Finish up with mix firmware && mix firmware.burn"
+echo "Finish up with mix firmware && mix burn"
