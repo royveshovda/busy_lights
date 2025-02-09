a791253b22f956f64761ae68291bc144230c8c66
Roy Veshovda <roy@veshovda.no>
2025-02-09 06:58:22 +0100

    ✨ Enable live dashboard always

--- ui/lib/busy_lights_ui_web/router.ex
+++ ui/lib/busy_lights_ui_web/router.ex
@@ -33,18 +33,18 @@ defmodule BusyLightsUiWeb.Router do
   # end
 
   # Enable LiveDashboard in development
-  if Application.compile_env(:busy_lights_ui, :dev_routes) do
+  #if Application.compile_env(:busy_lights_ui, :dev_routes) do
     # If you want to use the LiveDashboard in production, you should put
     # it behind authentication and allow only admins to access it.
     # If your application does not have an admins-only section yet,
     # you can use Plug.BasicAuth to set up some basic authentication
     # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
+  import Phoenix.LiveDashboard.Router
 
-    scope "/dev" do
-      pipe_through(:browser)
+  scope "/dev" do
+    pipe_through(:browser)
 
-      live_dashboard("/dashboard", metrics: BusyLightsUiWeb.Telemetry)
-    end
+    live_dashboard("/dashboard", metrics: BusyLightsUiWeb.Telemetry)
   end
+  #end
 end
